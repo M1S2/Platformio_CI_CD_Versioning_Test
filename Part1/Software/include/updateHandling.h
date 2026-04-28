0f9cf5500c263fab137095cb75b68199a34bf2c5
Markus Scheich <markus.scheich@gmx.de>
2026-04-28 23:50:58 +0200

    Next parts for updateHandling logic (update for FW and file system added, progress while updating not available)

--- Part1/Software/include/updateHandling.h
+++ Part1/Software/include/updateHandling.h
@@ -14,10 +14,13 @@ typedef struct update_info
 {
     bool valid;             // Whether the update info is valid (i.e. whether a valid manifest was fetched and parsed)
     String version;         // Firmware version (e.g. "1.0.0" or "dev-SW_v2.0.0-p2-856538c")
+    bool has_fs_update;     // Whether the update includes a filesystem update (i.e. whether the manifest contains a valid URL for the filesystem binary)
     String url_fw;          // Firmware URL
     String url_fs;          // Filesystem URL (if applicable, otherwise empty)
     String url_fw_sha256;   // SHA256 hash of the firmware binary for integrity check
     String url_fs_sha256;   // SHA256 hash of the filesystem binary for integrity check (if applicable, otherwise empty)
+    float updateProgress_fw; // Progress of the firmware update (0.0 to 100.0)
+    float updateProgress_fs; // Progress of the filesystem update (0.0 to 100.0)
 } update_info_t;
 
 extern UpdateChannel currentUpdateChannel;
@@ -26,6 +29,6 @@ extern update_info_t updateInfo;
 void updateHandling_initWebserverEndpoints();
 void updateHandling_loop();
 void updateHandling_startFetchingNewestVersionInfos();
-//bool otaPerformUpdate();
+void updateHandling_startUpdate();
 
 #endif
--- Part1/Software/src/updateHandling.cpp
+++ Part1/Software/src/updateHandling.cpp
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
 #include <ArduinoJson.h>
 #include "updateHandling.h"
 #include "wifiHandling.h"
@@ -32,6 +33,8 @@ Dev Manifest Format:
 
 update_info_t updateInfo;
 bool fetchNewestVersionInfos = false;
+bool isUpdating = false;
+bool currentlyUpdatingFs = false;
 
 /**********************************************************************/
 
@@ -86,9 +89,104 @@ bool updateHandling_fetchVersion(update_info_t &info)
     info.url_fs = String(baseUrl) + doc["part1_fs"].as<String>();
     info.url_fw_sha256 = String(baseUrl) + doc["part1_fw_sha256"].as<String>();
     info.url_fs_sha256 = String(baseUrl) + doc["part1_fs_sha256"].as<String>();
+    info.has_fs_update = (info.url_fs != "");
     info.valid = true;
 
     return info.valid;
+}
+
+/**********************************************************************/
+
+#warning TODO: check the SHA256 hash of the downloaded firmware and filesystem binaries before applying the update, to prevent bricking the device by applying a corrupted update (e.g. due to network issues during download)
+
+bool updateHandling_performUpdate()
+{
+    if (!updateInfo.valid) return false;
+
+    #ifdef DEBUG_OUTPUT
+        Serial.printf("Performing update to version %s\n", updateInfo.version.c_str());
+    #endif
+
+    WiFiClientSecure client;
+    client.setInsecure(); // TODO: remove this later...
+
+    ESPhttpUpdate.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    ESPhttpUpdate.setClientTimeout(10000);
+
+    currentlyUpdatingFs = false;
+
+    #ifdef DEBUG_OUTPUT
+        ESPhttpUpdate.onProgress([](int cur, int total)
+        {
+            float percent = (total > 0) ? (100.0f * cur / total) : 0.0f;
+            Serial.printf("Progress: %d / %d (%.2f%%)\n", cur, total, percent);
+            if(!currentlyUpdatingFs)
+            {
+                updateInfo.updateProgress_fw = percent;
+            }
+            else
+            {
+                updateInfo.updateProgress_fs = percent;
+            }
+        });
+    #endif
+
+    bool fsUpdateResult = true;
+    if(updateInfo.has_fs_update)
+    {
+        currentlyUpdatingFs = true;
+        #ifdef DEBUG_OUTPUT
+            Serial.println("Update file system...");
+        #endif
+        t_httpUpdate_return returnFsUpdate = ESPhttpUpdate.updateFS(client, updateInfo.url_fs);
+        switch (returnFsUpdate)
+        {
+            case HTTP_UPDATE_FAILED:
+                #ifdef DEBUG_OUTPUT
+                    Serial.printf("FS Update failed: %s\n", ESPhttpUpdate.getLastErrorString().c_str());
+                #endif
+                break;
+            case HTTP_UPDATE_NO_UPDATES:
+                #ifdef DEBUG_OUTPUT    
+                    Serial.println("No FS update available");
+                #endif
+                break;
+            case HTTP_UPDATE_OK:
+                #ifdef DEBUG_OUTPUT    
+                    Serial.println("FS Update ok");
+                #endif
+                break;
+        }
+        fsUpdateResult = (returnFsUpdate == HTTP_UPDATE_OK);
+        currentlyUpdatingFs = false;
+    }
+
+    bool fwUpdateResult = true;
+    #ifdef DEBUG_OUTPUT
+        Serial.println("Update firmware...");
+    #endif
+    t_httpUpdate_return returnFwUpdate = ESPhttpUpdate.update(client, updateInfo.url_fw);
+    switch (returnFwUpdate)
+    {
+        case HTTP_UPDATE_FAILED:
+            #ifdef DEBUG_OUTPUT
+                Serial.printf("Update failed: %s\n", ESPhttpUpdate.getLastErrorString().c_str());
+            #endif
+            break;
+        case HTTP_UPDATE_NO_UPDATES:
+            #ifdef DEBUG_OUTPUT    
+                Serial.println("No update available");
+            #endif
+            break;
+        case HTTP_UPDATE_OK:
+            #ifdef DEBUG_OUTPUT    
+                Serial.println("Update ok");
+            #endif
+            break;
+    }
+    fwUpdateResult = (returnFwUpdate == HTTP_UPDATE_OK);
+
+    return fsUpdateResult && fwUpdateResult;
 }
 
 /**********************************************************************/
@@ -99,6 +197,11 @@ void updateHandling_clearVersionInfo()
     updateInfo.version = "";
     updateInfo.url_fw = "";
     updateInfo.url_fs = "";
+    updateInfo.url_fw_sha256 = "";
+    updateInfo.url_fs_sha256 = "";
+    updateInfo.has_fs_update = false;
+    updateInfo.updateProgress_fw = 0.0f;
+    updateInfo.updateProgress_fs = 0.0f;
 }
 
 /**********************************************************************/
@@ -125,17 +228,27 @@ void updateHandling_initWebserverEndpoints()
         request->send(200, "text/plain", "Fetching...");
     });
 
+    server.on("/update/start", HTTP_GET, [](AsyncWebServerRequest *request)
+    {
+        updateHandling_startUpdate();
+        request->send(200, "text/plain", "Update started");
+    });
+
     server.on("/update/get_info", HTTP_GET, [](AsyncWebServerRequest *request)
     {
         DynamicJsonDocument doc(2048);
         doc["channel"] = (currentUpdateChannel == UPDATE_CHANNEL_STABLE) ? "stable" : "dev";
         doc["isFetching"] = fetchNewestVersionInfos;
+        doc["isUpdating"] = isUpdating;
         doc["available"] = updateInfo.valid;
+        doc["has_fs_update"] = updateInfo.has_fs_update;
         doc["version"] = updateInfo.version;
         doc["url_fw"] = updateInfo.url_fw;
         doc["url_fs"] = updateInfo.url_fs;
         doc["url_fw_sha256"] = updateInfo.url_fw_sha256;
         doc["url_fs_sha256"] = updateInfo.url_fs_sha256;
+        doc["updateProgress_fw"] = updateInfo.updateProgress_fw;
+        doc["updateProgress_fs"] = updateInfo.updateProgress_fs;
         String response;
         serializeJson(doc, response);
         request->send(200, "application/json", response);
@@ -150,6 +263,12 @@ void updateHandling_startFetchingNewestVersionInfos()
 
     // Set flag to fetch the newest version infos in the next loop() iteration, because the HTTP request handling should be as fast as possible and not block for too long (e.g. by waiting for the HTTP response from the update server)
     fetchNewestVersionInfos = true;
+}
+
+void updateHandling_startUpdate()
+{    
+    // Set flag to perform the update in the next loop() iteration, because the HTTP request handling should be as fast as possible and not block for too long (e.g. by waiting for the HTTP response from the update server)
+    isUpdating = true;
 }
 
 /**********************************************************************/
@@ -172,34 +291,9 @@ void updateHandling_loop()
         #endif
         fetchNewestVersionInfos = false;
     }
-}
-
-/**********************************************************************/
-
-/*
-bool otaPerformUpdate()
-{
-    if (firmwareUrl == "") return false;
-
-    WiFiClient client;
-
-    t_httpUpdate_return ret = ESPhttpUpdate.update(client, firmwareUrl);
-
-    switch (ret)
+    else if(isUpdating)
     {
-        case HTTP_UPDATE_FAILED:
-            Serial.printf("Update failed: %s\n", ESPhttpUpdate.getLastErrorString().c_str());
-            return false;
-
-        case HTTP_UPDATE_NO_UPDATES:
-            Serial.println("No update available");
-            return false;
-
-        case HTTP_UPDATE_OK:
-            Serial.println("Update ok");
-            return true;
+        updateHandling_performUpdate();
+        isUpdating = false;
     }
-
-    return false;
 }
-*/
